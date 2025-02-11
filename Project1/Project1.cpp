581fca09abdbf4a789201aad084723fb7d282078
RomanKob <roma.kobelckov@yandex.ru>
2025-02-11 22:48:37 +0300

    Чтение файла и подсчёт повторений
    Добавил чтение файла при помощи функции fopen, также реализовал подсчёт повторяющихся символов

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,9 +1,19 @@
-﻿// Project1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    
+    auto fr = fopen("Input", "rb");
+    fseek(fr, 0L, SEEK_END);
+    long lenght = ftell(fr);
+    fseek(fr, 0L, SEEK_SET);
+    cout << lenght << endl;
+    int freq[128] = { 0 };
+    for (int i = 0; i < lenght; i++)
+    {
+        freq[(unsigned char)fgetc(fr)]++;
+    }
+    fclose(fr);
+
 }
